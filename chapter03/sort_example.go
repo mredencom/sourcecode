f0a146cd30107a9b145289e4c7ecacb97b07b6fa
MrEden <mreden@163.com>
2019-11-25 10:21:22 +0800

    sort and interface

--- chapter03/sort_example.go
+++ chapter03/sort_example.go
@@ -9,7 +9,9 @@ import (
 func main() {
 	//SortExample()
 	//SearchSortExample()
-	GuessingGame()
+	//GuessingGame()
+	//IntSliceExample()
+	InterfaceSortExample()
 }
 
 //排序方式的实现
@@ -58,9 +60,10 @@ func (s StuScores) Swap(i, j int) {
 }
 
 // 这里数组必须是排序的
-func SearchSortExample()  {
+func SearchSortExample() {
 	x := 16
-	s := []int{3, 6, 8, 11, 45} // 注意已经升序排序
+	// TODO:注意已经升序排序 这里的数组必须是升序排序好的数组
+	s := []int{3, 6, 8, 11, 45}
 	pos := sort.Search(len(s), func(i int) bool { return s[i] >= x })
 	if pos < len(s) && s[pos] == x {
 		fmt.Println(x, " 在 s 中的位置为：", pos)
@@ -68,6 +71,7 @@ func SearchSortExample()  {
 		fmt.Println("s 不包含元素 ", x)
 	}
 }
+
 // 查询
 func GuessingGame() {
 	var s string
@@ -78,4 +82,51 @@ func GuessingGame() {
 		return s != "" && s[0] == 'y'
 	})
 	fmt.Printf("Your number is %d.\n", answer)
+}
+
+// int类型的slice进行排序
+func IntSliceExample() {
+	s := []int{1, 23, 2, 4, 5, 4, 0}
+	sort.Ints(s)
+	fmt.Println("正序：", s)
+	// TODO search方法必须是正序
+	x := sort.SearchInts(s, 1)
+	fmt.Println("正序找到数据：", x)
+	sort.Sort(sort.Reverse(sort.IntSlice(s)))
+	fmt.Println("倒序：", s)
+}
+
+// 使用扩展的排序 interface
+// TODO 这个是可以排序结构体的排序方式
+func InterfaceSortExample() {
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Gopher", 7},
+		{"Alice", 55},
+		{"阿呆", 24},
+		{"阿信", 75},
+	}
+	sort.Slice(people, func(i, j int) bool {
+		//return people[i].Age < people[j].Age
+		return people[i].Name < people[j].Name
+	})
+
+	fmt.Println("Sort by age or name:", people)
+	fmt.Println(sort.SliceIsSorted(people, func(i, j int) bool {
+		return people[i].Age < people[j].Age
+	}))
+
+	a := []int{2, 3, 4, 200, 100, 21, 234, 56}
+	x := 21
+
+	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })           // 升序排序
+	index := sort.Search(len(a), func(i int) bool { return a[i] >= x }) // 查找元素
+
+	if index < len(a) && a[index] == x {
+		fmt.Printf("found %d at index %d in %v\n", x, index, a)
+	} else {
+		fmt.Printf("%d not found in %v,index:%d\n", x, a, index)
+	}
 }
